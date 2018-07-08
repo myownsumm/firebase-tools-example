2b2df5ed16292c4fda0181ac64762fcda2bf6fd2
alexander.poltoratskiy <myownsumm@gmail.com>
2018-07-08 21:30:00 +0300

    register component prepared

--- src/app/modules/auth/components/register/register.component.ts
+++ src/app/modules/auth/components/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'auth-register',
@@ -6,7 +8,29 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
-    ngOnInit() {
+    registerForm: FormGroup;
 
+    constructor(protected fb: FormBuilder, protected router: Router) {
+
+    }
+
+    ngOnInit() {
+        this.initRegisterForm();
+    }
+
+    initRegisterForm() {
+        this.registerForm = this.fb.group({
+            email: ['', Validators.required],
+            password: ['', Validators.required],
+            password_confirmation: ['', Validators.required]
+        });
+    }
+
+    goToLogin() {
+        return this.router.navigate(['/auth', 'login']);
+    }
+
+    register() {
+        // todo
     }
 }
