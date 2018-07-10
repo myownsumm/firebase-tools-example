0168a1ea80ea08158d392a28a31ea281f74474fc
alexander.poltoratskiy <myownsumm@gmail.com>
2018-07-10 18:50:32 +0300

    clean from not used actions types

--- src/app/modules/auth/store/auth.actions.ts
+++ src/app/modules/auth/store/auth.actions.ts
@@ -4,11 +4,6 @@ import * as firebase from 'firebase/app';
 import { IAuthUser } from '../../../../typings';
 
 export const LOG_IN_ATTEMPT_ACTION = '[auth] LOG_IN_ATTEMPT_ACTION';
-export const REDIRECT_TO_LOG_IN_ACTION = '[auth] REDIRECT_TO_LOG_IN_ACTION';
-export const LOG_OUT_ACTION = '[auth] LOG_OUT_ACTION';
-export const SUCCESS_LOGGED_IN_ACTION = '[auth] SUCCESS_LOGGED_IN_ACTION';
-export const USER_ALREADY_AUTHENTICATED_ACTION = '[auth] USER_ALREADY_AUTHENTICATED_ACTION';
-
 export const REGISTER_ATTEMPT_ACTION = '[auth] REGISTER_ATTEMPT_ACTION';
 
 export class LogInAttemptAction implements Action {
@@ -27,28 +22,3 @@ export class RegisterAttemptAction implements Action {
     }
 }
 
-export class RedirectToLogInAction implements Action {
-    readonly type = REDIRECT_TO_LOG_IN_ACTION;
-
-    constructor(public payload?: string) {
-
-    }
-}
-
-export class LogOutAction implements Action {
-    readonly type = LOG_OUT_ACTION;
-}
-
-export class SuccessLoggedInAction implements Action {
-    readonly type = SUCCESS_LOGGED_IN_ACTION;
-
-    constructor(public payload: { firebaseUser: firebase.User, adminUser: IAuthUser }) {
-    }
-}
-
-export class UserAlreadyAuthenticatedAction implements Action {
-    readonly type = USER_ALREADY_AUTHENTICATED_ACTION;
-
-    constructor(public payload: { firebaseUser: firebase.User, adminUser: IAuthUser }) {
-    }
-}
--- src/app/modules/auth/store/auth.reducer.ts
+++ src/app/modules/auth/store/auth.reducer.ts
@@ -1,6 +1,5 @@
 import {
-    LOG_IN_ATTEMPT_ACTION, LOG_OUT_ACTION, REDIRECT_TO_LOG_IN_ACTION, SUCCESS_LOGGED_IN_ACTION,
-    USER_ALREADY_AUTHENTICATED_ACTION
+    LOG_IN_ATTEMPT_ACTION
 } from './auth.actions';
 import * as firebase from 'firebase/app';
 import * as _ from 'lodash';
@@ -18,26 +17,6 @@ export function authReducer(state: IAuthState = INITIAL_AUTH_STATE, action: any)
     let newState = _.cloneDeep(state);
 
     switch (action.type) {
-        case SUCCESS_LOGGED_IN_ACTION:
-            newState.firebaseUser = action.payload.firebaseUser;
-
-            return newState;
-
-        case LOG_IN_ATTEMPT_ACTION:
-            return newState;
-
-        case REDIRECT_TO_LOG_IN_ACTION:
-            return newState;
-
-        case LOG_OUT_ACTION:
-            newState.firebaseUser = undefined;
-            return newState;
-
-        case USER_ALREADY_AUTHENTICATED_ACTION:
-            newState.firebaseUser = action.payload.firebaseUser;
-
-            return newState;
-
         default:
             return newState;
     }
